2e62b27d0f83361d5d3a03f826151c2ffeef61e2
AleemK15 <stonekhan45@gmail.com>
2023-05-06 14:46:21 +0300

    second push
    Added new features

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,4 +1,13 @@
 const taskList = document.getElementById('task-list');
+const allTask = document.getElementById("all-task");
+const completed = document.getElementById("complete");
+const remaining = document.getElementById("remaining");
+
+let taskCount = 0;
+let remainingCount = 0;
+let completedCount = 0;
+
+
 
 function addTask(event) {
     event.preventDefault();
@@ -8,31 +17,59 @@ function addTask(event) {
     const taskItem = createTaskItem(task);
     taskList.appendChild(taskItem);
     input.value = '';
+    taskCount++;
+    remainingCount++;
+    allTask.innerText = taskCount;
+    remaining.innerText = remainingCount;
 }
 
 function createTaskItem(task) {
     const taskItem = document.createElement('li');
     taskItem.classList.add('flex', 'justify-between', 'items-center', 'py-2');
     taskItem.innerHTML = `
-            <span class="flex-grow ml-16 cursor-pointer">${task}</span>
+            <li class="flex-grow ml-16 cursor-pointer">${task}</li>
             <button class="ml-4 bg-red-500 text-white px-3 py-3 rounded hover:bg-red-300">Delete</button>
             <button class="ml-4 mr-4 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-300">Edit</button>
         `;
     taskItem.querySelector('.bg-red-500').addEventListener('click', deleteTask);
     taskItem.querySelector('.bg-blue-500').addEventListener('click', editTask);
-    taskItem.querySelector('.flex-grow').onclick = function(){
-      this.classList.toggle("line-through");
-      this.classList.toggle("text-green-800");
-    }
+    taskItem.querySelector('.flex-grow').addEventListener('click', completeTask);
     return taskItem;
 }
 
 function deleteTask(event) {
     event.preventDefault();
     const taskItem = event.target.parentNode;
-    taskList.removeChild(taskItem);
+    taskList.removeChild(taskItem); 
+    const myTask= taskItem.querySelector('.flex-grow');
+        if(myTask.classList.contains("line-through")) {
+            completedCount--;
+        } else {
+            remainingCount--;
+        }
+    taskCount--;
+    allTask.innerText = taskCount;
+    completed.innerText = completedCount;
+    remaining.innerText = remainingCount;
 }
 
+function completeTask(event){
+    event.preventDefault();
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('line-through');
+        event.target.classList.toggle("text-green-800");
+        if (event.target.classList.contains('line-through')) {
+          remainingCount--;
+          completedCount++;
+        } else {
+          remainingCount++;
+          completedCount--;
+        }
+        remaining.innerText = remainingCount;
+        completed.innerText = completedCount;
+      }
+}
+ 
 function editTask(event) {
     event.preventDefault();
     const taskItem = event.target.parentNode;
@@ -63,8 +100,3 @@ function editTask(event) {
 }
 
 document.querySelector('form').addEventListener('submit', addTask);
-
-
-
-
-
